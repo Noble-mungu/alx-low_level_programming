839590c0a4735a036d5b72ef1e351d9ee1f11bd3
Noble Mungu <mungunoble@gmail.com>
2022-03-22 20:28:10 +0300

    add files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * 
+ * return Always 0
+ */
+
+int main(void)
+{
+    char *str;
+    int len;
+    str = "My first strlen!"
+    printf("%d\n",len);
+    return(0);
+    
+}
